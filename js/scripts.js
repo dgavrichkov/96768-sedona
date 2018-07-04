25aad0955cb81d35a0a0cfd399c5c5c690ec15ff
DitmarGrim <dgavrichkov32@gmail.com>
2018-07-04 17:44:39 +0300

    сделал разворачивающийся попап

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@
 // 	modal.classList.toggle('search__popup--off');
 // }
 
-
-
+document.querySelector('#modal-trigger')
+	.addEventListener('click', function(){
+		this.nextElementSibling
+			.classList.toggle('search__popup--on');
+	})
 
 
