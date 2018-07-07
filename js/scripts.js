1a1824d9e32758c95b4909cf622c7296f2c0a6bb
DitmarGrim <dgavrichkov32@gmail.com>
2018-07-07 18:17:45 +0300

    добавлены шрифты woff, уточнены отступы

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ var popup = document.querySelector('.search__popup');
 
 var trigger = document.querySelector('#modal-trigger');
 var form = document.querySelector('#order-form');
-var datein = popup.querySelector("[name=date-in");
-var dateoff = popup.querySelector("[name=date-off");
-var numadult = popup.querySelector("[name=adult-num");
-var numchild = popup.querySelector("[name=child-num");
+var datein = popup.querySelector('[name=date-in]');
+var dateoff = popup.querySelector('[name=date-off]');
+var numadult = popup.querySelector('[name=adult-num]');
+var numchild = popup.querySelector('[name=child-num]');
 
 
 trigger.addEventListener('click', function(){
@@ -17,7 +17,7 @@ trigger.addEventListener('click', function(){
 form.addEventListener('submit', function(evt){
 	if(!datein.value || !dateoff.value || !numadult.value || !numchild.value){
 		evt.preventDefault();
-		console.log("Заполните все поля");
+		console.log('Заполните все поля');
 		popup.classList.add('search__popup--error')
 	}
 })
